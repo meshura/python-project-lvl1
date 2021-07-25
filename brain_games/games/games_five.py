de64983b1c63369ace2077cde293f20e31ee1415
Meshura <shtraus@inbox.ru>
2021-07-25 14:26:26 +0300

    Changes to be committed: 	new file:   brain_games/games/games_five.py 	new file:   brain_games/scripts/brain_prime.py 	modified:   pyproject.toml

---
+++ brain_games/games/games_five.py
@@ -0,0 +1,34 @@
+import prompt
+from random import randint
+from brain_games import cli
+from brain_games import logic
+
+
+def game5_logic(number1_1):
+    var = []
+    for index in range(number1_1):
+        if index == 0:
+            continue
+        if number1_1 % index == 0:
+            var.append(index)
+    if len(var) == 1:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def game_5():
+    cli.hello()
+    print("Hello, " + cli.name + "!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    i = 0
+    while i < 3:
+        number = randint(1, 3571)
+        question = game5_logic(number)
+        exam = "Question: {}"
+        print(exam.format(str(number)))
+        answer = prompt.string('Your answer: ')
+        if logic.logic(answer, question) is None:
+            return "Let's try again, " + cli.name
+        i += 1
+    return 'Congratulations, ' + cli.name + '!'
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from brain_games.games import games_five
+
+
+def main():
+    print(games_five.game_5())
+
+
+if __name__ == '__main__':
+    main()
