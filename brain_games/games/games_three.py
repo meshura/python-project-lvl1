81493289fba598f8a40b5192eaf7c269cb9e0a7e
Meshura <shtraus@inbox.ru>
2021-07-25 11:28:47 +0300

    Changes to be committed: 	modified:   brain_games/games/games_three.py 	modified:   brain_games/scripts/brain_gcd.py

--- brain_games/games/games_three.py
+++ brain_games/games/games_three.py
@@ -4,22 +4,26 @@ from brain_games import cli
 from brain_games import logic
 
 
-def game3_logic(random_number1, random_number2):
+def game3_logic_2(var1_1, var2_2):
+    var3 = []
+    if len(var1_1) > len(var2_2):
+        var1_1, var2_2 = var2_2, var1_1
+    for j in var1_1:
+        if j in var2_2:
+            var3.append(j)
+    return var3
+
+
+def game3_logic_1(random_number1, random_number2):
     var1 = []
     var2 = []
-    var3 = []
     for index1 in range(1, random_number1 + 1):
         if random_number1 % index1 == 0:
             var1.append(index1)
     for index2 in range(1, random_number2 + 1):
         if random_number2 % index2 == 0:
             var2.append(index2)
-    if len(var1) > len(var2):
-        var1, var2 = var2, var1
-    for j in var1:
-        if j in var2:
-            var3.append(j)
-    return max(var3)
+    return max(game3_logic_2(var1, var2))
 
 
 def game_3():
@@ -30,7 +34,7 @@ def game_3():
     while i < 3:
         random_number1_1 = randint(1, 100)
         random_number2_2 = randint(1, 100)
-        question = game3_logic(random_number1_1, random_number2_2)
+        question = game3_logic_1(random_number1_1, random_number2_2)
         exam = "Question: {} {}"
         print(exam.format(str(random_number1_1), str(random_number2_2)))
         answer = prompt.string('Your answer: ')
