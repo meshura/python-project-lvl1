16c1363e7fbbda2baeac97508a380281d0f79099
Meshura <shtraus@inbox.ru>
2021-07-25 13:35:37 +0300

    Changes to be committed: 	new file:   brain_games/games/games_four.py 	new file:   brain_games/scripts/brain_progression.py 	modified:   pyproject.toml

---
+++ brain_games/games/games_four.py
@@ -0,0 +1,26 @@
+import prompt
+from random import randint
+from brain_games import cli
+from brain_games import logic
+
+
+def game_4():
+    cli.hello()
+    print("Hello, " + cli.name + "!")
+    print('What number is missing in the progression?')
+    i = 0
+    while i < 3:
+        start_prog = randint(1, 20)
+        increase_prog = randint(1, 10)
+        stop_prog = start_prog + (10 * increase_prog)
+        shadow_number = randint(0, 9)
+        prog = list(range(start_prog, stop_prog, increase_prog))
+        question = prog.pop(shadow_number)
+        prog.insert(shadow_number, '..')
+        exam = "Question: {}"
+        print(exam.format(str(prog)))
+        answer = prompt.string('Your answer: ')
+        if logic.logic(answer, question) is None:
+            return "Let's try again, " + cli.name
+        i += 1
+    return 'Congratulations, ' + cli.name + '!'
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from brain_games.games import games_four
+
+
+def main():
+    print(games_four.game_4())
+
+
+if __name__ == '__main__':
+    main()
